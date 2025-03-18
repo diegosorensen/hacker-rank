10d1b1661d8cbc11a69ea9d69b1f1e3cd2db94b8
Diegosor Sorensen <diego.sorensen@gmail.com>
2025-03-18 17:49:51 -0300

    adding 1 more

---
+++ src/main/java/problemsolving/implementation/Sorting/SolutionFindtheMedian.java
@@ -0,0 +1,58 @@
+package src.main.java.problemsolving.implementation.Sorting;
+
+
+
+import java.io.*;
+        import java.math.*;
+        import java.security.*;
+        import java.text.*;
+        import java.util.*;
+        import java.util.concurrent.*;
+        import java.util.function.*;
+        import java.util.regex.*;
+        import java.util.stream.*;
+        import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class ResultFindtheMedian {
+
+    /*
+     * Complete the 'findMedian' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int findMedian(List<Integer> arr) {
+        // Write your code here
+
+        List<Integer> lis ;
+        Collections.sort(arr);
+        int mitad = arr.size()/2;
+
+
+        return arr.get(mitad);
+    }
+
+}
+
+public class SolutionFindtheMedian {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int result = ResultFindtheMedian.findMedian(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
